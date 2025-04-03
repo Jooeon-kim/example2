836450b72c84c9ad3df75f49156839dbcce05e6c
wogh397 <wogh397@gmail.com>
2025-04-03 18:06:06 +0900

    장재호_04.03_intro

--- src/intro.js
+++ src/intro.js
@@ -1,11 +1,11 @@
 import { Link, useNavigate } from "react-router-dom";
-import jj from "./img/japan2.jpg"
+import jj from "./img/non_Logo.jpg"
 function Intro (){
     const navigate = useNavigate();
     return (
         <>
         <img src={jj} height="200" width="400" onClick={()=>navigate("/menu")}></img>
-        <h1>어서오세요 김&&장 식당에</h1>
+        <h1></h1>
         
         </>
     )
