788e8172444752c80741f46921dc31a55c258795
jangs-uxui <jangs.uxui@gmail.com>
2025-04-08 17:26:14 +0900

    vipResult

--- src/VipResult.js
+++ src/VipResult.js
@@ -48,16 +48,16 @@ function VipResult() {
             </div>
           );
         })} */}
-        <div className="order_num">주문번호 <em>{orderNumber}</em></div>
         <div className="total">
           <span>결제금액</span>
-          <strong>{totalPrice}</strong>
+          <strong>{new Intl.NumberFormat('ko-KR').format(totalPrice)} 원</strong>
         </div>
         <p className="point">
           <span>{vip.name}</span>님&nbsp;
           <span>잔여포인트</span>
-          <em>{vip.point} point</em>
+          <em>{new Intl.NumberFormat('ko-KR').format(vip.point)} point</em>
         </p>
+        <div className="order_num">주문번호 <em>{orderNumber}</em></div>
 
         <div className="count_wrap"><span className="notice">{countdown}초 후 메인화면으로 돌아갑니다.</span></div>
 
