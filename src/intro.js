3e527e57e4d4b9b55ac1a256f9f0f486a582a9d6
jangs-uxui <jangs.uxui@gmail.com>
2025-04-08 15:22:48 +0900

    intro,mainSlice

--- src/intro.js
+++ src/intro.js
@@ -3,15 +3,26 @@ import jj from "./img/non_Logo.png";
 import { useSelector } from "react-redux";
 function Intro() {
   const navigate = useNavigate();
-  const best = useSelector((state)=> state.main.bestMenu);
+  const best = useSelector((state) => state.main.bestMenu);
   const randomIndex = Math.floor(Math.random() * 4) + 1;
   const randomImagePath = `/image/intro/kimjang${randomIndex}.png`;
   return (
-    <div className="index">
-      <img src={randomImagePath} onClick={() => navigate("/menu/drink")} />
-      <h1>인트로</h1>
-      <h1>베스트메뉴!</h1>
-      {best.map((e)=>(<div><img src={e.src}></img><div>{e.name}</div></div>))}
+    <div className="intro" onClick={() => navigate("/menu/drink")}>
+
+      <div className="intro_box">
+        <h1>김&&장 Cafe</h1>
+        <img src={randomImagePath} className="intro_img" />
+        <div className="enter">주문하기</div>
+      </div>
+
+      <h2>베스트메뉴!</h2>
+      {best.map((e) => (
+        <div className="best_box">
+          <img src={e.src} className="best_img" />
+          <span className="title">{e.name}</span>
+        </div>
+      ))}
+
     </div>
   );
 }
